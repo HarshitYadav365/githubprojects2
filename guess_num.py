37427bab6067e5c8b32245beb6653fe65634d30e
Harshit Yadav <140739372+HarshitYadav365@users.noreply.github.com>
2023-09-17 18:13:06 +0530

    Add files via upload

---
+++ guess_num.py
@@ -0,0 +1,16 @@
+import random
+
+max = 10
+n = random.randint(1, max)
+print('Guess the number from 1 to %d' % max)
+guess = None
+while guess != n:
+    guess = int(input('Your try: '))
+    if guess > n:
+        print('you lose')
+        break
+    if guess < n:
+        print('you lose')
+        break
+    if guess == n:
+        print("you win the game")
